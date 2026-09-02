b617dcd4eb8ce84d0a50d3d353bc461526d8e986
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-09-01 21:54:44 -0300

    expanding currency conversion problem

---
+++ ruby/currency_conversion.rb
@@ -0,0 +1,27 @@
+class Numeric
+	@@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+	def method_missing(method_id)
+		singular_currency = method_id.to_s.gsub( /s$/, '')
+
+		if @@currencies.has_key?(singular_currency)
+			Money.new(self, singular_currency, @@currencies)
+		else
+			super
+		end
+
+	end
+end
+
+class Money
+	def initialize(value, currency, currencies)
+		@value = value
+		@currency = currency
+		@currencies = currencies
+	end
+
+	def in(currency_to_convert)
+		singular_currency = method_id.to_s.gsub( /s$/, '')
+		value_in_dollars = @value * @currencies[@currency]
+		value_in_dollars / @currencies[singular_currency]
+	end
+end
