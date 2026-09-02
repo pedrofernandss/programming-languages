d9493f8f6da9963c2d48fb8aaaefae5637fc38bd
Pedro Fernandes <pedrofernandes.olv@gmail.com>
2026-09-01 21:54:23 -0300

    fixing regex problem

--- ruby/palindrome.rb
+++ ruby/palindrome.rb
@@ -1,9 +1,6 @@
 def palindrome?(string)
 	lowercase_string = string.downcase
-
-    only_characters = lowercase_string.gsub(/\w, '')
-    
-
+    only_characters = lowercase_string.gsub(/\W/, '')
     reserverd_only_character = only_character.reverse
 
     if only_character == reserverd_only_character
